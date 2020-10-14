58bff61ce1e926efde918f297b04cb80cca9a3c1
cboatman <caseyb574@gmail.com>
2020-10-14 16:01:25 -0500

    Updates

---
+++ precache-manifest.19da94fe2134c7d490860dde0ff6ed46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681643cc6ae72c9b2d65abd708203987",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "ed677b703f1dfb225d4e",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "b38166e664ab12d11c83",
+    "url": "/box-office/static/js/2.46e57f53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.46e57f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed677b703f1dfb225d4e",
+    "url": "/box-office/static/js/main.f893dab7.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
