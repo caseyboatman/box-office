12790d4a2f4bfa51323e8ba0db78454bc545dbd1
cboatman <caseyb574@gmail.com>
2020-10-14 16:05:35 -0500

    Updates

--- precache-manifest.f7631e156456e338c3ebe6da95f69d7f.js
+++ precache-manifest.f7631e156456e338c3ebe6da95f69d7f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "681643cc6ae72c9b2d65abd708203987",
+    "revision": "976740bd04cc663f38626223601036ca",
     "url": "/box-office/index.html"
   },
   {
-    "revision": "ed677b703f1dfb225d4e",
+    "revision": "4caf7244ba92654d1ea2",
     "url": "/box-office/static/css/main.93052401.chunk.css"
   },
   {
-    "revision": "b38166e664ab12d11c83",
-    "url": "/box-office/static/js/2.46e57f53.chunk.js"
+    "revision": "87772e393b3f93984dcd",
+    "url": "/box-office/static/js/2.c394080f.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/box-office/static/js/2.46e57f53.chunk.js.LICENSE.txt"
+    "url": "/box-office/static/js/2.c394080f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ed677b703f1dfb225d4e",
-    "url": "/box-office/static/js/main.f893dab7.chunk.js"
+    "revision": "4caf7244ba92654d1ea2",
+    "url": "/box-office/static/js/main.a79be2b1.chunk.js"
   },
   {
     "revision": "a24731daa79adae5d2cf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/box-office/precache-manifest.19da94fe2134c7d490860dde0ff6ed46.js"
+  "/box-office/precache-manifest.f7631e156456e338c3ebe6da95f69d7f.js"
 );
 
 self.addEventListener('message', (event) => {
